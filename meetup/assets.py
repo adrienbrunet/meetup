26eaa03c5b2604b829c9f789ec0880f9c09622c7
Adrien <abrunet@centrale-marseille.fr>
2015-11-30 12:37:26 +0100

    Adding assets in app

---
+++ meetup/assets.py
@@ -0,0 +1,35 @@
+from django_assets import Bundle, register
+
+
+css_main = Bundle(
+    'bootstrap/dist/css/bootstrap.css',
+    'djangular/css/styles.css',
+    'font-awesome/css/font-awesome.css',
+
+    filters=('cssrewrite', 'yui_css',),
+    output='compiled-assets/gen/css_main.$(version)s.css'
+)
+
+register('css_main', css_main)
+
+js_main = Bundle(
+    'jquery/dist/jquery.min.js',
+    'lodash/lodash.min.js',
+    'angular/angular.min.js',
+    'angular-bootstrap/ui-bootstrap-tpls.min.js',
+    'angular-route/angular-route.min.js',
+    'angular-resource/angular-resource.min.js',
+    'angular-cookies/angular-cookies.min.js',
+    'angular-animate/angular-animate.min.js',
+
+    'djangular/js/django-angular.js',
+
+    'js/angular-app/dashboard/module.js',
+    'js/angular-app/dashboard/resources.js',
+    'js/angular-app/dashboard/controllers.js',
+    'js/angular-app/dispatch.js',
+
+    filters='closure_js',
+    output='compiled-assets/gen/js_main.%(version)s.js')
+
+register('js_main', js_main)
